9c9d46d7899a9892c1a618e32eb2a19750df342d
thuang79 <thuang79@my.bcit.ca>
2024-04-05 15:58:12 -0700

    Responsiveness
    changed the styling to be responsive in mobile and desktop

--- src/app/model/project.ts
+++ src/app/model/project.ts
@@ -1,17 +1,19 @@
 import { Category } from './category';
 
 export interface Project {
-    id: number;
-    title: string;
-    slug: string;
-    excerpt: string;
-    body: string;
-    url: string | null;
-    published_date: string | null;
-    image: string | null;
-    thumb: string | null;
-    created_at: string;
-    updated_at: string;
-    category: Category | null;
-    tags: any;
-  }  
+  id: number;
+  title: string;
+  slug: string;
+  excerpt: string;
+  body: string;
+  url: string | null;
+  published_date: string | null;
+  image: string | null;
+  image2: string | null; 
+  image3: string | null;  
+  thumb: string | null;
+  created_at: string;
+  updated_at: string;
+  category: Category | null;
+  tags: any;
+}
