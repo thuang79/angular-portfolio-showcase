1438252bc63c5b141137a0f4319d85ce00483afb
thuang79 <thuang79@my.bcit.ca>
2024-04-05 14:33:49 -0700

    added homepage and about page, styling
    added homepage and about page, styling

--- src/app/data/tags.ts
+++ src/app/data/tags.ts
@@ -1,7 +1,7 @@
 import { Tag } from '../model/tag';
 
 export const TAGS: Tag[] = [
-    { id:1, name: 'MVC', slug: 'mvc'},
+    { id:1, name: 'C#', slug: 'c#'},
     { id:2, name: 'Javascript', slug: 'javascript'},
     { id:3, name: 'React', slug: 'react'},
     { id:4, name: 'CSS', slug: 'css'},
