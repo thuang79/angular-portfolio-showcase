2adeef180801f12deb28d830fe19a05fd67b5266
thuang79 <thuang79@my.bcit.ca>
2024-04-06 23:35:53 -0700

    fixed filtering for categories and styled

--- src/app/components/tags/tags.component.ts
+++ src/app/components/tags/tags.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { Tag } from '../../model/tag';
-// import { TAGS } from '../data/tags';
 import { TagService } from '../../services/tag.service';
 
 @Component({
@@ -13,10 +12,8 @@ import { TagService } from '../../services/tag.service';
   styleUrl: './tags.component.scss',
 })
 export class TagsComponent {
-  // tags = TAGS;
   constructor(private tagService: TagService) {}
 
-  // categories = CATEGORIES;
   tags: Tag[] = [];
   getTags(): void {
     this.tags = this.tagService.getTags();
